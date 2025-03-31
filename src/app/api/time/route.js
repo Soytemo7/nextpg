2227942aa50a2858f1de6ea20f107da8dc3e32c6
Soytemo7 <contador.manuel0@gmail.com>
2025-03-31 00:50:33 -0600

    force

--- src/app/api/time/route.js
+++ src/app/api/time/route.js
@@ -1,6 +1,8 @@
 import prisma from "@/libs/prisma";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
     const result = await prisma.$queryRaw`SELECT NOW()`;
     return NextResponse.json({
